f1f8e9174a12c766c59ff02f4a102fe2283b3d63
agendafacilpremium <nannymeuamor16@gmail.com>
2025-05-11 22:10:02 -0300

    Delete routes directory

--- routes/testRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/teste', (req, res) => {
-  res.json({ mensagem: 'Rota de teste funcionando!' });
-});
-
-module.exports = router;
